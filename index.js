856f8d8a55af23752aa6dedd4084120cdad79f73
Renz Torres <wr3ns@protonmail.com>
2022-06-28 11:46:45 -0400

    Added YAML and YAML front matter

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const fs = require('fs');
+const fm = require('front-matter');
 
 const prm = {
     src: "./src/",
@@ -11,15 +12,19 @@ const prm = {
     dist: "./dist/"
 }
 
-const container = fs.readFileSync(prm.src + prm.components + "container.html", 'utf-8');
-const header = fs.readFileSync(prm.src + prm.components + "header.html", 'utf-8');
-const footer = fs.readFileSync(prm.src + prm.components + "footer.html", 'utf-8');
+let container = fs.readFileSync(prm.src + prm.components + "container.html", 'utf-8');
+let header = fs.readFileSync(prm.src + prm.components + "header.html", 'utf-8');
+let footer = fs.readFileSync(prm.src + prm.components + "footer.html", 'utf-8');
 
-const index = fs.readFileSync(prm.src + prm.content + "index.html", 'utf-8');
+const content = [];
+
+let index = fs.readFileSync(prm.src + prm.content + "index.html", 'utf-8');
 
 const dom = new JSDOM(container);
+
 const headerHTML = JSDOM.fragment(header);
 const footerHTML = JSDOM.fragment(footer);
+
 const indexHTML = JSDOM.fragment(index);
 
 const wrapperNode = dom.window.document.querySelector("#wrapper");
