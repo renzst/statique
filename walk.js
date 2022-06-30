e7a5e4fa1279c9caa542eaf6d9fca9f775f2c4f3
Renz Torres <wr3ns@protonmail.com>
2022-06-30 10:24:58 -0400

    Helper function from SO

---
+++ walk.js
@@ -0,0 +1,27 @@
+/* 
+https://stackoverflow.com/questions/5827612/node-js-fs-readdir-recursive-directory-search
+Victor Powell
+With some edits for ES6
+*/
+
+const fs = require('fs');
+const path = require('path');
+
+const walk = (dir) => {
+    let results = [];
+    const list = fs.readdirSync(dir);
+    list.forEach(file => {
+        file = path.join(dir, file);
+        const stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) { 
+            /* Recurse into a subdirectory */
+            results = results.concat(walk(file));
+        } else { 
+            /* Is a file */
+            results.push(file);
+        }
+    });
+    return results;
+}
+
+exports.walk = walk;
