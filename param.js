74d122787c97c9fc8d99b2226ca7d23f930d31a1
Renz Torres <wr3ns@protonmail.com>
2022-06-30 10:25:06 -0400

    param change

--- param.js
+++ param.js
@@ -1,7 +1,8 @@
 const param = {
-    src: "./src/content/",
-    dist: "./dist/",
-    components: "./src/components/"
+    src: "src/content/",
+    dist: "dist/",
+    components: "src/components/",
+    titlePrefix: "Renz Torres | "
 }
 
 exports.param = param;
