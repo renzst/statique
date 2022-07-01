9fbbbd6f14979c73b8fb19acdb359e948af90ab2
Renz Torres <wr3ns@protonmail.com>
2022-07-01 15:01:15 -0400

    added carousel parameters

--- param.js
+++ param.js
@@ -2,7 +2,16 @@ const param = {
     src: "src/content/",
     dist: "dist/",
     components: "src/components/",
-    titlePrefix: "Renz Torres | "
+    titlePrefix: "Renz Torres | ",
+    carousels: {
+        projects: {
+            dir: "projects/",
+            id: "projects",
+            label: "Projects",
+            desc: "A summary of my major projects",
+            level: 0
+        }
+    }
 }
 
 exports.param = param;
